5c9545d1d791c09dad6b99ecb26ff7c87575c26d
Алексей Киринюк <alexey.kirinuk@yandex.ru>
2017-05-17 20:21:37 +0700

    Start wrote api and views.

--- Kernel/Managers/ProjectsManager.cs
+++ Kernel/Managers/ProjectsManager.cs
@@ -1,30 +1,25 @@
 ﻿using Projects.Context;
 using Projects.DataTypes;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Projects.Managers
 {
     public class ProjectsManager
     {
-        public IEnumerable<Project> GetAll()
+        public readonly string[] Includes = new string[] { "Employee", "Leader" };
+
+        public IEnumerable<Project> GetAll() => Operation((projects) => projects.ToList());
+        public Project Find(long id) => Operation((projects) => projects.FirstOrDefault(p => id == p.Id));
+
+        private TDataType Operation<TDataType>(Func<IEnumerable<Project>, TDataType> action)
         {
-            var result = default(IEnumerable<Project>);
+            var result = default(TDataType);
 
             using (var context = new CurrentContext())
             {
-                result = context.ProjectsBase as IEnumerable<Project>;
-            }
-
-            return result;
-        }
-
-        public Project Find(long id)
-        {
-            var result = default(Project);
-
-            using (var context = new CurrentContext())
-            {
-                result = context.ProjectsBase.Find(id);
+                result = action(context.ProjectsBase.Include("Employee").Include("Leader"));
             }
 
             return result;
---
+++ Projects/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Projects.DataTypes;
+using Projects.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Projects.Server.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public ActionResult Index() => View();
+
+        public IEnumerable<Project> GetAll() => new ProjectsManager().GetAll();
+
+    }
+}
